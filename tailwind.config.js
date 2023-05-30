0a364b5b733cd5b32190ebcfdb775b9bf0353697
dmarcosc <carcharoth92@gmail.com>
2023-05-30 19:07:43 +0200

    project built

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    colors: {
+      'primary-color': '#ffff',
+      'secondary-color': '#4e4e50',
+      'highlight-color': '#c3073f',
+      'bg-color': '#1a1a1d',
+    },
+    fontFamily: {
+      sansserif: ['sans-serif'],
+      helvetica: ['Helvetica Neue'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
